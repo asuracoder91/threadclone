631994b9eed1b85cba2bf2bce3963eb5896b0bd1
asuracoder91 <asuracoder@gmail.com>
2023-09-22 20:59:35 +0900

    broken?

--- lib/main/view/search_page.dart
+++ lib/main/view/search_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -8,6 +9,7 @@ import 'package:thread/main/widgets/search_block.dart';
 
 import '../../repos/dark_mode_provider.dart';
 import '../model/fake_data.dart';
+import '../widgets/article_block.dart';
 
 class SearchPage extends ConsumerStatefulWidget {
   const SearchPage({super.key});
@@ -19,7 +21,23 @@ class SearchPage extends ConsumerStatefulWidget {
 }
 
 class _SearchPageState extends ConsumerState<SearchPage> {
+  final TextEditingController _searchController = TextEditingController();
   final searchArticles = fakeDataSearch;
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(() {
+      print(_searchController.text);
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,26 +64,75 @@ class _SearchPageState extends ConsumerState<SearchPage> {
                   ),
                 ),
                 Gaps.v10,
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
                   child: CupertinoSearchTextField(
-                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                    controller: _searchController,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 10),
                   ),
                 ),
                 Gaps.v10,
-                ListView.builder(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: searchArticles.length,
-                    itemBuilder: (context, index) {
-                      return SearchBlock(
-                        avatar: searchArticles[index].avatar,
-                        name: searchArticles[index].name,
-                        blueCheck: searchArticles[index].blueCheck,
-                        account: searchArticles[index].account,
-                        followers: searchArticles[index].followers,
-                      );
-                    })
+                Container(
+                  decoration: BoxDecoration(
+                    color: ref.watch(darkModeProvider)
+                        ? Colors.black
+                        : Colors.white,
+                  ),
+                  child: StreamBuilder(
+                    stream: (_searchController.text.isEmpty)
+                        ? FirebaseFirestore.instance
+                            .collection('images')
+                            .orderBy('createdAt', descending: true)
+                            .snapshots()
+                        : FirebaseFirestore.instance
+                            .collection('images')
+                            .where('text',
+                                arrayContains: _searchController.text)
+                            .orderBy('createdAt', descending: true)
+                            .snapshots(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
+                            snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      final docs = snapshot.data!.docs;
+                      return ListView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: docs.length,
+                          itemBuilder: (context, index) {
+                            return Column(
+                              children: [
+                                Container(
+                                  color: Colors.white,
+                                  child: ArticleBlock(
+                                    avatar: docs[index]['avatar'],
+                                    name: docs[index]['name'],
+                                    blueCheck: docs[index]['blueCheck'],
+                                    time: docs[index]['time'],
+                                    text: docs[index]['text'],
+                                    images: List<String>.from(
+                                        docs[index]['images']),
+                                    replies: docs[index]['replies'],
+                                    replyAvatars: List<String>.from(
+                                        docs[index]['replyAvatars']),
+                                    likes: docs[index]['likes'],
+                                  ),
+                                ),
+                                Gaps.v6,
+                                const Divider(
+                                  thickness: 0.5,
+                                ),
+                              ],
+                            );
+                          });
+                    },
+                  ),
+                )
               ],
             ),
           ),
